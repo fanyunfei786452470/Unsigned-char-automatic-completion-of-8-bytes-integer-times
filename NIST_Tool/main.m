c14092c393b6fe20f4e40c816e1312f9b53e1c8d
范云飞 <786452470@qq.com>
2017-11-09 20:41:16 +0800

    进一步封装，unsigned char 更加方便的转化为 unsigned long long

--- NIST_Tool/main.m
+++ NIST_Tool/main.m
@@ -8,8 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
-void unsignedchar2(unsigned char * input,unsigned char * output,int len );
-void unsignedchar1(unsigned char * input,unsigned char * output,int len );
+void unsignedchar2(unsigned char * input, unsigned long long * output, int len );
+void unsignedchar1(unsigned char * input, unsigned long long * output, int len );
 void unsignedchar(unsigned char * input, unsigned long long * output, int len);
 void unsignedcharFromUnsignedlonglong(unsigned long long * input, unsigned char * output, int len);
 
@@ -19,45 +19,45 @@ int main(int argc, const char * argv[])
     {
         
 #pragma mark - 8的整数倍
-        unsigned char input[] = {0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77};
-        int total = sizeof(input);
-        unsigned long long output[total/8];
+        //        unsigned char input[] = {0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77};
+        //        int total = sizeof(input);
+        //        unsigned long long output[total/8];
+        //
+        //        //8的整数倍
+        //        if (total/8 >= 1 && (total%8) == 0)
+        //        {
+        //            unsignedchar(input, output, total);
+        //        }
         
-        //8的整数倍
-        if (total/8 >= 1 && (total%8) == 0)
-        {
-            unsignedchar(input, output, total);
-        }
-
 #pragma mark - 小于8的数
-//        unsigned char input[] = {0x00,0x11,0x22,0x33,0x44,0x55,0x66};
-//        int total = sizeof(input);
-//        unsigned char output[8];
-
-//        //小于8的数
-//        if (total/8 == 0)
-//        {
-//            unsignedchar1(input, output, total);
-//        }
-//        
+        //        unsigned char input[] = {0x00,0x11,0x22,0x33,0x44,0x55,0x66};
+        //        int total = sizeof(input);
+        //        unsigned long long output[1];
+        //        //小于8的数
+        //        if (total/8 == 0)
+        //        {
+        //            unsignedchar1(input, output, total);
+        //        }
+        //
         
 #pragma mark 大于8的数
-//        unsigned char input[] = {0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x33,0x44};
-//        int total = sizeof(input);
-//        unsigned char output[16];
-//        
-//        //大于8的数
-//        if (total/8 >= 1 && (total%8) != 0)
-//        {
-//            unsignedchar2(input, output, total);
-//        }
-
+        unsigned char input[] = {0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x33,0x44,0x55,0x66,0x33,0x44};
+        int total = sizeof(input);
+        unsigned long long output[total/8 + 1];
+        
+        //大于8的数
+        if (total/8 >= 1 && (total%8) != 0)
+        {
+            unsignedchar2(input, output, total);
+        }
+        
     }
     return 0;
 }
 
-void unsignedchar2(unsigned char * input,unsigned char * output,int len )
+void unsignedchar2(unsigned char * input,unsigned long long  * output,int len )
 {
+    unsigned char temp[8 * (len/8 + 1)];
     //在此需要补充 8 - total%8 个字节
     for (int i = 0; i < len; i++)
     {
@@ -70,16 +70,18 @@ void unsignedchar2(unsigned char * input,unsigned char * output,int len )
     {
         memcpy(buchong + i, chong, 1);
     }
-    memcpy(output, input, len);
-    memcpy(output + len, buchong, 8- len%8);
-    
-    for (int i = 0; i < 16; i++)
+    memcpy(temp, input, len);
+    memcpy(temp + len, buchong, 8- len%8);
+    memcpy(output, temp, 8 * (len/8 + 1));
+    for (int i = 0; i < (len/8 + 1); i++)
     {
-        printf("%hhx\n",output[i]);
+        printf("%llx\n",output[i]);
     }
 }
-void unsignedchar1(unsigned char * input,unsigned char * output,int len )
+
+void unsignedchar1(unsigned char * input, unsigned long long * output,int len )
 {
+    unsigned char tempput[8];
     //在此需要补充 8 - total 个字节的0x00
     for (int i = 0; i < len; i++)
     {
@@ -92,14 +94,14 @@ void unsignedchar1(unsigned char * input,unsigned char * output,int len )
     {
         memcpy(buchong + i, chong, 1);
     }
-    memcpy(output, input, len);
+    memcpy(tempput, input, len);
     memcpy(input + len, buchong, 8 - len);
-    
-    for (int i = 0; i < len; i++)
+    memcpy(output, tempput, 8);
+    for (int i = 0; i < 1; i++)
     {
-        printf("%hhx\n",output[i]);
+        printf("%llx\n",output[i]);
     }
-
+    
 }
 
 void unsignedchar(unsigned char * input, unsigned long long * output, int len)
